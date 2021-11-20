793ab7fa4ec87cc1a870a6862bcaadb22093a0ef
ce2kettu <teemu.ket@gmail.com>
2021-11-20 14:53:43 +0200

    Add not found page

---
+++ src/components/NotFound.js
@@ -0,0 +1,27 @@
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const NotFound = () => {
+    return (
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography variant="h2" color="inherit" noWrap>
+                    404 Not Found
+                </Typography>
+                <Typography variant="h6" color="inherit" noWrap>
+                    {"We could not find the page you're looking for! :("}
+                </Typography>
+            </Box>
+        </Container>
+    )
+}
+
+export default NotFound;
