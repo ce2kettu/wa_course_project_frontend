c8060bbe7bce8851776b0d050adbb9874f897798
ce2kettu <teemu.ket@gmail.com>
2021-12-04 13:34:02 +0200

    Add user header, which is displayed in posts and comments

---
+++ src/components/UserHeader.js
@@ -0,0 +1,31 @@
+import { Avatar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { stringAvatar } from "../util";
+import { useNavigate } from 'react-router-dom';
+
+const UserHeader = ({ user }) => {
+    let navigate = useNavigate();
+
+    const openProfile = () => {
+        navigate(`/profile/${user._id}`);
+    }
+
+    return (
+        <Box
+            onClick={openProfile}
+            sx={{
+                cursor: 'pointer',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                mb: 2
+            }}>
+            <Avatar {...stringAvatar(user.displayName || 'Unknown')} />
+            <Typography sx={{ ml: 2, mb: 0 }} variant="subtitle2" gutterBottom component="div">
+                {user.displayName || 'Unknown'}
+            </Typography>
+        </Box>
+    )
+}
+
+export default UserHeader;
