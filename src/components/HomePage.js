f68dc2256c0c8a7f035d82f4d179a03b83b6cfd9
ce2kettu <teemu.ket@gmail.com>
2021-12-04 16:33:18 +0200

    Changed homepage content display

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,108 +1,141 @@
 import { useEffect, useState, useContext, Fragment } from 'react';
-import { Alert, List, ListItem, ListItemText, Box, Fab, Paper, Divider, Typography } from '@mui/material';
+import { Alert, List, ListItem, ListItemText, Stack, Fab, Paper, Divider, Typography, Grid, Box, Avatar } from '@mui/material';
 import { Add as AddIcon } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../reducers/auth';
 import Config from '../config';
+import { stringAvatar } from '../util';
+import UserHeader from './UserHeader';
 
 const HomePage = () => {
-    const initialState = {
-        posts: [],
-        errorMessage: null,
-        isFetching: true,
+  const initialState = {
+    posts: [],
+    errorMessage: null,
+    isFetching: true,
+  }
+
+  const [data, setData] = useState(initialState);
+  const { state: authState } = useContext(AuthContext);
+  let navigate = useNavigate();
+
+  // Fetch all posts
+  useEffect(() => {
+    let isMounted = true;
+
+    fetch(`${Config.apiUrl}/api/posts/all`)
+      .then(res => res.json())
+      .then(data => {
+        if (isMounted) {
+          setData({ ...data, isFetching: false, posts: data.posts });
+        }
+      })
+      .catch(err => setData({
+        ...data, isFetching: false,
+        errorMessage: 'Could not fetch posts. Please try again.'
+      }));
+
+    return () => {
+      isMounted = false;
+    }
+  }, []);
+
+  const openPost = (post) => {
+    navigate(`/post/${post._id}`);
+  }
+
+  const createPost = () => {
+    navigate('/createPost');
+  }
+
+  return <>
+    <Stack direction={{ xs: 'column', sm: 'row' }} alignItems={{ xs: 'flex-start', sm: 'center' }}>
+      <Typography gutterBottom component="div" variant="h3" sx={{ mt: 2, mb: 3 }}>
+        All Questions
+      </Typography>
+      {
+        authState.isAuthenticated &&
+        <Fab variant="extended" color="secondary" sx={{ ml: { xs: 0, sm: 5 }, mb: { xs: 3, sm: 0 } }} onClick={createPost}>
+          <AddIcon sx={{ mr: 1 }} />
+          ask question
+        </Fab>
+      }
+    </Stack>
+
+    {/* Display error if posts could not be fetched */}
+    {data.errorMessage &&
+      <Alert severity="error" sx={{ mt: 1 }}>{data.errorMessage}</Alert>
     }
 
-    const [data, setData] = useState(initialState);
-    const { state: authState } = useContext(AuthContext);
-    let navigate = useNavigate();
-
-    // Fetch all posts
-    useEffect(() => {
-        let isMounted = true;
-
-        fetch(`${Config.apiUrl}/api/posts/all`)
-            .then(res => res.json())
-            .then(data => {
-                if (isMounted) {
-                    setData({ ...data, isFetching: false, posts: data.posts });
-                }
-            })
-            .catch(err => setData({
-                ...data, isFetching: false,
-                errorMessage: 'Could not fetch posts. Please try again.'
-            }));
-
-        return () => {
-            isMounted = false;
-        }
-    }, []);
-
-    const openPost = (post) => {
-        navigate(`/post/${post._id}`);
+    {/* No posts available */}
+    {
+      !data.isFetching && data.posts.length === 0 && !data.errorMessage &&
+      <Alert severity="info" sx={{ mt: 1 }}>No posts available.</Alert>
     }
 
-    const createPost = () => {
-        navigate('/createPost');
+    {/* Display loading status */}
+    {
+      data.isFetching &&
+      <Alert severity="info" sx={{ mt: 1 }}>Loading posts...</Alert>
     }
 
-    return <>
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center'
-        }}>
-            <Typography gutterBottom component="div" variant="h3" sx={{ mt: 2, mb: 3 }}>
-                Posts
-            </Typography>
-            {
-                authState.isAuthenticated &&
-                <Fab variant="extended" color="secondary" sx={{ ml: 5 }} onClick={createPost}>
-                    <AddIcon sx={{ mr: 1 }} />
-                    New post
-                </Fab>
-            }
-        </Box>
-
-        {/* Display error if posts could not be fetched */}
-        {data.errorMessage &&
-            <Alert severity="error" sx={{ mt: 1 }}>{data.errorMessage}</Alert>
-        }
-
-        {/* No posts available */}
-        {
-            !data.isFetching && data.posts.length === 0 && !data.errorMessage &&
-            <Alert severity="info" sx={{ mt: 1 }}>No posts available.</Alert>
-        }
-
-        {/* Display loading status */}
-        {
-            data.isFetching &&
-            <Alert severity="info" sx={{ mt: 1 }}>Loading posts...</Alert>
-        }
-
-        {/* Display posts */}
-        {
+    {/* Display posts */}
+    {
+      data.posts.length > 0 &&
+      <Paper elevation={3}>
+        <List>
+          {
             data.posts.length > 0 &&
-            <Paper variant="outlined">
-                <List>
-                    {
-                        data.posts.length > 0 &&
-                        data.posts.map((post, index) =>
-                            <Fragment key={index}>
-                                <ListItem onClick={() => openPost(post)} sx={{ cursor: 'pointer' }}>
-                                    <ListItemText primary={post.title} secondary={post.body}></ListItemText>
-                                </ListItem>
-                                {
-                                    index !== data.posts.length - 1 &&
-                                    <Divider />
-                                }
-                            </Fragment>
-                        )
-                    }
-                </List>
-            </Paper>
-        }
-    </>
+            data.posts.map((post, index) =>
+              <div key={index}>
+                <Grid container spacing={2} sx={{ py: 1, px: 2, cursor: 'pointer' }} onClick={() => openPost(post)}>
+                  <Grid item xs="auto" alignItems="center">
+                    <Stack>
+                      <Box onClick={(e) => e.stopPropagation()}>
+                        <UserHeader user={post.user} />
+                      </Box>
+                      <Typography
+                        sx={{ cursor: 'pointer' }}
+                        gutterBottom
+
+                        variant="subtitle1"
+                        component="div"
+                      >
+                        {post.title}
+                      </Typography>
+                    </Stack>
+                  </Grid>
+                  <Grid item xs>
+                    <Stack alignItems="flex-end" sx={{ mt: 1 }}>
+                      <Box>
+                        <Typography variant="subtitle1" component="div" display="inline" color="secondary">
+                          {post.score}
+                        </Typography>
+                        <Typography variant="subtitle1" component="div" display="inline" sx={{ ml: .5 }}>
+                          votes
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <Typography variant="subtitle1" component="div" display="inline" color="secondary">
+                          {post.comments.length}
+                        </Typography>
+                        <Typography variant="subtitle1" component="div" display="inline" sx={{ ml: .5 }}>
+                          answers
+                        </Typography>
+                      </Box>
+                    </Stack>
+                  </Grid>
+                </Grid>
+                {
+                  index !== data.posts.length - 1 &&
+                  <Divider />
+                }
+              </div>
+            )
+          }
+        </List>
+      </Paper>
+    }
+  </>
 }
 
 export default HomePage;
