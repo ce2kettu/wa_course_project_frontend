4b7db651aa657e3fe07816ff6d46327cb081e56e
ce2kettu <teemu.ket@gmail.com>
2021-12-04 19:47:21 +0200

    Redirect after asking a question, display status messages

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,14 +1,20 @@
 import { useContext, useState } from 'react';
 import { AuthContext } from '../reducers/auth';
+import { SnackbarContext } from '../SnackbarContext';
+import { Button, TextField, Typography, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { createMessage } from '../util';
 import MDEditor from '@uiw/react-md-editor';
 import Config from '../config';
-import { Button, TextField, Typography, Box } from '@mui/material';
 
 const CreatePost = () => {
   const { state: authState } = useContext(AuthContext);
+  const { snackPack, setSnackPack } = useContext(SnackbarContext);
   const [body, setBody] = useState('');
   const [title, setTitle] = useState('');
+  let navigate = useNavigate();
 
+  // Send request to server to create new question
   const newPost = () => {
     fetch(`${Config.apiUrl}/api/posts/new`, {
       method: 'POST',
@@ -21,6 +27,16 @@ const CreatePost = () => {
         body
       })
     })
+      .then(res => res.json())
+      .then(data => {
+        if (data.success) {
+          setSnackPack((prev) => [...prev, createMessage('Question submitted!', 'success')]);
+          navigate(`/q/${data.post._id}`);
+        } else {
+          setSnackPack((prev) => [...prev, createMessage('Could not submit question.', 'error')]);
+        }
+      })
+      .catch(err => setSnackPack((prev) => [...prev, createMessage('Please try again.', 'error')]))
   };
 
   return (
@@ -42,7 +58,7 @@ const CreatePost = () => {
 
       <MDEditor
         textareaProps={{
-          placeholder: 'Post content ....'
+          placeholder: 'Write your question...'
         }}
         value={body}
         onChange={setBody}
@@ -52,7 +68,7 @@ const CreatePost = () => {
         authState.isAuthenticated
         && (
           <Box sx={{ mt: 3 }}>
-            <Button variant="outlined" onClick={newPost}>Create post</Button>
+            <Button variant="outlined" onClick={newPost}>Submit</Button>
           </Box>
         )
       }
