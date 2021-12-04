453a70477350bd0556a503e8eda5a159359081c6
ce2kettu <teemu.ket@gmail.com>
2021-12-04 13:33:39 +0200

    Add profile component

---
+++ src/components/UserProfile.js
@@ -0,0 +1,68 @@
+import { Alert, Avatar, Container, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { useEffect, useState, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import Config from '../config';
+import { stringAvatar } from '../util';
+
+const UserProfile = () => {
+  const [error, setError] = useState('');
+  const [user, setUser] = useState(null);
+  let params = useParams();
+
+  // Fetch profile
+  useEffect(() => {
+    let isMounted = true;
+
+    fetch(`${Config.apiUrl}/api/users/profile/${params.userId}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.success) {
+          if (isMounted) {
+            document.title = `${data.profile.displayName} - Heapoverflow`;
+            setUser(data.profile);
+          }
+        } else {
+          setError('Could not fetch user profile.');
+        }
+      })
+      .catch(err => setError('Could not fetch user profile.'));
+
+    return () => {
+      isMounted = false;
+    }
+  }, []);
+
+  return (
+    <Container sx={{ mt: 5 }}>
+      {error && (
+        <Alert severity="error">{error}</Alert>
+      )}
+
+      {
+        user &&
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Avatar {...stringAvatar(user.displayName || 'Unknown')} sx={{ width: 72, height: 72 }} />
+          <Typography sx={{ mt: 5 }} variant="subtitle2" gutterBottom component="div">
+            Display name: {user.displayName || 'Unknown'}
+          </Typography>
+          <Typography sx={{ mt: 5 }} variant="subtitle2" gutterBottom component="div">
+            Registered at: {user.createdAt}
+          </Typography>
+          <Typography sx={{ mt: 5 }} variant="subtitle2" gutterBottom component="div">
+            Bio: {user.createdAt}
+          </Typography>
+        </Box>
+      }
+    </Container>
+  )
+}
+
+export default UserProfile;
